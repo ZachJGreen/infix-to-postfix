48701bd816a6165fcdae1737e017f6a9f1e8c1cf
robotiques <green.zach.j@gmail.com>
2023-09-10 22:35:30 -0500

    created test GUI

--- my_ui.py
+++ my_ui.py
@@ -1,33 +1,34 @@
-import sys
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import *
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Sprint 0 test GUI")
+window.geometry("640x480")
+
+button = tk.IntVar()
+
+#Text
+test_label1 = tk.Label(text="this is example text")
+test_label1.pack()
 
 
-class MainWindow(QMainWindow):
+#Lines
+canvas = tk.Canvas(window, width=300, height = 220)
+canvas.pack()
+y = 20
+for i in range(0,4):
+    canvas.create_line(0, y, 400, y, fill="green", width = 2)
+    y += 20
 
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("test")
+#checkbox
+check = tk.Checkbutton(window, text="I'm a checkbox")
+check.pack()
 
-        expression = QLineEdit(self)
-        expression.setPlaceholderText("Enter an expression")
-        expression.setAlignment()
-        layout = QVBoxLayout()
-        widgets = [
-            QLabel,
-            QLineEdit,
-            QPushButton,
-            QRadioButton,
-        ]
+#radio buttons
+radio1 = tk.Radiobutton(window, text="Radio button 1", value=1, variable=button)
+radio2 = tk.Radiobutton(window, text="Radio button 2", value=2, variable=button)
+radio1.pack()
+radio2.pack()
 
-        for w in widgets:
-            layout.addWidget(w())
-        widget = QWidget()
-        widget.setLayout(layout)
-
-        self.setCentralWidget(widget)
-
-app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
-window.show()
-app.exec()
+test_label2 = tk.Label(window)
+test_label2.pack()
+window.mainloop()
